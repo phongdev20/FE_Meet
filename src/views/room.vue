799bf207336883e9588e3f57667bfb973a395cef
Thăng Đức <thang_td>
2022-12-10 10:14:35 +0700

    first commit

---
+++ src/views/room.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div v-for="(item, index) in message" :key="index">{{ item }}</div>
+    <input type="text" v-model="input" />
+    <button @click="handleSendMessage">Send</button>
+    <button @click="leave">leave</button>
+    <button @click="stream('getUserMedia')">stream cam</button>
+    <button @click="stream('getDisplayMedia')">stream Screen</button>
+    <div class="video">
+      <div class="video-grid" ref="video-grid"></div>
+    </div>
+  </div>
+</template>
